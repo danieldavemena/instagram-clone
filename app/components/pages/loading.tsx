2b0e6b1eccea67bb5129bf6ad35cb28d547ce6ae
danieldavemena <danieldanieldanieldave@gmail.com>
2025-03-22 01:01:08 +0800

    modified:   .gitignore 	modified:   app/components/pages/loading.tsx 	modified:   app/globals.css 	modified:   app/page.tsx 	modified:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   tsconfig.json

--- app/components/pages/loading.tsx
+++ app/components/pages/loading.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { Suspense } from "react";
+import ReactPlayer from "react-player";
+// import BackgroundVideo from "next-video/background-video";
+// import loadinganimation from "@/videos/Loading.mp4";
 
 const loading = () => {
-  return <div></div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const playVideo = () => {
+      if (videoRef.current) {
+        videoRef.current.play().catch((err) => {
+          console.error("Autoplay failed:", err);
+        });
+      }
+    };
+
+    if (document.readyState === "complete") {
+      playVideo();
+    } else {
+      window.addEventListener("load", playVideo);
+    }
+
+    return () => window.removeEventListener("load", playVideo);
+  }, []);
+
+  return (
+    <main>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center w-[600px]">
+          {/* <BackgroundVideo
+            loop={false}
+            src={loadinganimation}
+            autoPlay
+            className="object-cover"
+          /> */}
+          <video ref={videoRef} autoPlay playsInline muted>
+            <source src="/loading/Loading.mp4" />
+          </video>
+          <p className="text-center text-zinc-400 text-sm  transform -translate-y-10">
+            This project is an independent UI clone made for educational
+            purposes only and is not affiliated with Instagram or Meta
+            Platforms, Inc. All trademarks and assets belong to their respective
+            owners.
+          </p>
+          <div className="loader"></div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Home from "./components/pages/home";
 import Loading from "./components/pages/loading";
 
 const page = () => {
-  return (
-    <div>
-      <Home />
-      {/* <Loading /> */}
-    </div>
-  );
+  const [page, setPage] = useState(<Loading />);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPage(<Home />);
+    }, 5000);
+  });
+  return <div>{page}</div>;
 };
 
 export default page;
